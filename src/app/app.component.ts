a2436b68663ca4e1dd88263af03561eaa6cf5178
Tharindu Jayawardhana <tharinduj@embla.asia>
2024-03-01 04:00:00 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  price = 100;
+  discount = 5;
+
+}
