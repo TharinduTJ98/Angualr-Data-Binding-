b2b5b74dab20042975d32de41741ede70c9d2432
Tharindu Jayawardhana <tharinduj@embla.asia>
2024-03-01 04:04:40 +0530

    Create precentage customer pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PrecentagePipe } from './pipe/precentage.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PrecentagePipe,
     
   ],
   imports: [
